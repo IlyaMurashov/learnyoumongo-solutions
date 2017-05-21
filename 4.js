0739baa73edfe89d9535db689570fb3148d7c838
IlyaMurashov <murashov.ilya@gmail.com>
2017-05-21 16:49:36 +0200

    4

---
+++ 4.js
@@ -0,0 +1,24 @@
+const mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://localhost:27017/learnyoumongo', (err, db) => {
+  if (err) {
+    return console.err(err);
+  }
+
+  const parrots = db.collection('parrots');
+  parrots.find({
+    age: { $gt: parseInt(process.argv[2]) }
+  }, {
+    name: 1,
+    age: 1,
+    _id: 0
+  }).toArray((err, docs) => {
+    if (err) {
+      return console.err(err);
+    }
+
+    console.log(docs);
+  });
+
+  db.close();
+});
